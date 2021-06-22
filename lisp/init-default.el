d90fada18b24081f667dc865bf19b64688b72f27
“mk0417” <pengdata1@gmail.com>
2021-06-22 17:41:17 +0100

    put back after delete

--- lisp/init-default.el
+++ lisp/init-default.el
@@ -20,6 +20,17 @@
 (setq trash-directory "~/.Trash"
       delete-by-moving-to-trash t)
 
+;; https://christiantietze.de/posts/2021/06/emacs-trash-file-macos/
+(when (memq window-system '(mac ns))
+  (defun system-move-file-to-trash (path)
+    "Moves file at PATH to the macOS Trash according to `move-file-to-trash' convention.
+Relies on the command-line utility 'trash' to be installed.
+Get it from:  <http://hasseg.org/trash/>"
+    (shell-command (concat "trash -vF \"" path "\"" "| sed -e 's/^/Trashed: /'")
+                   nil ;; Name of output buffer
+                   "*Trash Error Buffer*")))
+
+
 (global-set-key (kbd "C-x k")   'kill-this-buffer)
 (global-set-key (kbd "C-x K")   'kill-buffer-and-window)
 (global-set-key (kbd "C-w")     'backward-kill-word)
